9fe28938e82a0e407cd5f9e7830583bb177f92e3
patrickzebinghe <patrick2hb@hotmail.com>
2017-10-19 18:13:15 -0400

    Initial Commit

---
+++ content.js
@@ -0,0 +1,111 @@
+// globals
+loc = window.location.href;
+chatList = '.chat-lines'
+usernameClass = 'from'
+lineClass = 'message-line'
+go = false
+currentChat = []
+nth = 0
+console.log(currentChat);
+// check if twitch is in beta
+if (loc.indexOf('go.twitch.tv') > 0) {
+	console.log("Twitch is in beta")
+	go = true
+	chatList = '.chat-list'
+	lineClass = 'chat-line__message'
+	usernameClass = 'chat-line__message--username'
+}
+
+// wait until specific element exists and call a callback on it
+function wait_exist(element, wait_time, callback) {
+  if (document.querySelector(element) !== null) {
+    console.log(document.querySelector(element))
+    callback(document.querySelector(element));
+    return;
+  } else {
+	console.log("Chat not found waiting another " + wait_time.toString() + " milliseconds.")
+    setTimeout(() => {
+      wait_exist(element, wait_time,callback);
+    }, wait_time)
+  }
+}
+
+// test to see what is in our storage
+function checkStorage(url){
+  chrome.storage.local.get(null, function(data){
+    x =window.location.href
+  });
+}
+
+// save the chat and save it. Also send a message to popup
+function sendUpdate(chat) {
+	data = {};
+	
+	currentChat = currentChat.concat(chat);
+	data[loc] = {'data': currentChat};
+	console.log(data)
+	console.log(JSON.parse(JSON.stringify(data)))
+	if (typeof data === 'object') {
+		try{
+			chrome.storage.local.set(JSON.parse(JSON.stringify(data)));
+		}
+		catch(error){
+			console.log(error);
+		}
+	}
+}
+
+function getChat(){ 
+    var lines = document.getElementsByClassName(lineClass);
+	console.log(lines.length)
+    chat= [];
+    for (var i = lines.length-1; i > 0; i--) {
+		var line = lines[i];
+		
+		if ((line.innerHTML.indexOf('Top Cheer by ') > -1) || (line.className.indexOf('special-message') > -1)){
+			continue;
+		} else if (line.getAttribute('checked')){
+			break;
+		} else {
+			
+			line.setAttribute('checked', 'checked');
+			if (line.children.length == 0) {
+			  continue;
+			}
+			var username = line.getElementsByClassName('from')[0].innerHTML;
+			if (go){
+			  var message = document.querySelectorAll('div[class="' + lineClass + '"] > span[data-a-target="chat-message-text"]')[i].innerHTML;
+			} else {
+			  var message = line.getElementsByClassName('message')[0].textContent.trim() || line.getElementsByClassName('message')[0].innerText.trim();
+			}
+			var time = line.getElementsByClassName('timestamp')[0].innerHTML;
+			
+			data = {'time': time, 'username': username, 'message':message.replace(/(\r\n|\n|\r)/gm,"").replace(/ +/g, ' ')};
+			chat.push(data);
+		}
+    }
+    nth = lines.length;
+    return chat;
+}
+
+function main() {
+  wait_exist(chatList, 1000, (element) => {
+    sendUpdate(getChat());
+    checkStorage(loc);
+    setInterval(() => {
+      sendUpdate(getChat());
+	  console.log('updated');
+    }, 3000);
+  });
+}
+
+chrome.storage.local.clear();
+main();
+
+
+
+
+
+
+
+
---
+++ popup.js
@@ -0,0 +1,129 @@
+//globals
+currentUsers = []
+
+function addMessages(messages){
+	//add messages to the chat box
+	for (var i in messages) {
+		var line = messages[i];
+		var box = document.getElementById('chat-box');
+
+		var container = document.createElement('div');
+		container.className += ' message-line ' + line.username
+		container.id = box.children.length + 1
+		var time = document.createElement('span');
+		time.innerHTML = line.time + ' ';
+		var user = document.createElement('span');
+		user.innerHTML = line.username + ': ';
+		
+		var message = document.createElement('span');
+		message.innerHTML = line.message;
+		
+		var split = line.message.split(' ');
+		var directed = []
+		for (var i in split) {
+			var part = split[i];
+			if (part.indexOf('@') == 0) {
+				directed.push(part);
+			}
+		}
+		var directed = directed[0]
+		if (directed) {
+			directed = directed.slice(1)
+		}
+		console.log(directed)
+		container.appendChild(time);
+		container.appendChild(user);
+		container.appendChild(message);
+		var u = line.username
+		container.addEventListener('click', function(us,d) {
+			return function() {filter(us,d);}
+		}(u,directed));
+		box.appendChild(container);
+		console.log('New message added!')
+	}
+	while (box.children.length > 700) {
+		box.removeChild(box.children[0])
+	}
+}
+
+function filter(usernames,directed){
+	var chat = document.getElementById('chat-box');
+	var directChat = document.getElementById('direct');
+	
+	directChat.innerHTML = ''
+	
+	var sender = Array.from(chat.getElementsByClassName(usernames));
+	var dirSender = Array.from(chat.getElementsByClassName(directed));
+	var allSender = sender.concat(dirSender);
+	
+	allSender = allSender.sort((a,b) => {
+		return  a.id - b.id;
+	})
+	
+	for (var i = 0; i < allSender.length; i++) {
+		console.log('adding message to filter');
+		
+		var mes = allSender[i];
+		
+		
+		var clone = mes.cloneNode(1)
+		directChat.appendChild(clone);
+	}
+
+}
+
+
+function start(){
+	chrome.tabs.query({"active": true, "currentWindow": true}, (tab) => {
+		
+		tab = tab[0];
+		if (tab.url.indexOf('twitch.tv') < 0){
+			return;
+		}
+		console.log('asdfasdf');
+		chrome.storage.local.get(tab.url, (data) => {
+		  if (data[tab.url]) {
+			var box = document.getElementById('chat-box');
+			addMessages(data[tab.url]['data']);
+			
+			box.scrollTop = box.scrollHeight;
+			chrome.storage.onChanged.addListener((change, place) => {
+				var diffs = change[tab.url]
+				chrome.storage.local.get(tab.url, (data) => {
+					var chatState = data[tab.url]['data']
+					if (chatState.length > 700) {
+						while (chatState.length > 700) {
+							chatState = chatState.splice(1);
+						}
+						var data = {}
+						data[tab.url] = {'data': chatState};
+						chrome.storage.local.set(data);
+					}
+				})
+				if (diffs){
+					
+					var newMessages = [];
+					nv = diffs.newValue.data;
+					ov = diffs.oldValue.data;
+
+					for (var i = nv.length-1; i > ov.length-1; i--){
+						newMessages.push(nv[i]);
+					}
+					addMessages(newMessages);
+					
+					box.scrollTop = box.scrollHeight;
+				}
+			});
+		  } else {
+			  setTimeout(() => {
+				  console.log('Nothing found trying again in a second');
+				  start();
+			  }, 1000 )
+			}
+		});
+	});
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	start();
+});
